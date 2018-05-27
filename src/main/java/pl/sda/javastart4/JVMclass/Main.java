9b2a42299c82c0bc39bd8b3cc54f298f12e551d7
Matuesz <mateusz.olewinski96@gmail.com>
2018-05-27 12:26:42 +0200

    init

---
+++ src/main/java/pl/sda/javastart4/JVMclass/Main.java
@@ -0,0 +1,22 @@
+package pl.sda.javastart4.JVMclass;
+//        C:\Users\Mateusz Olewiński\IdeaProjects\Javastart4\src\main\java\pl\sda\javastart4\Main.javar
+import java.util.stream.IntStream;
+
+public class Main {
+
+        //Kompilacja programu
+        //javac Main.java
+        //Uruchomienie
+        //java Main // (UWAGA)
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        for (int i = 0; i < 100; i++) {
+
+        }
+        IntStream stream = IntStream.builder().add(10).add(10).build();
+        int sum = stream.sum();
+        System.out.println(sum);
+    }
+}
